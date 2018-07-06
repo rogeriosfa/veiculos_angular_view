ba674e904d31ec6ce9e5426462005545832771d5
Rogério Rodrigues <rogeriosfa@gmail.com>
2018-07-06 02:51:11 -0300

    service and style

--- src/app/core/services/veiculos.service.ts
+++ src/app/core/services/veiculos.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import { VeiculosModel } from '../models/veiculos.model';
+import { VeiculoModel } from '../models/veiculo.model';
 import { MessageService } from './message.service';
 
 const httpOptions = {
@@ -11,7 +11,7 @@ const httpOptions = {
 
 @Injectable()
 export class VeiculosService {
-    public path = "http://localhost:8000/api/veiculos";
+    public url = "http://localhost:8000/api/veiculos";
 
     constructor(
         private http: HttpClient,
@@ -19,21 +19,65 @@ export class VeiculosService {
     ) { }
 
 
-    getVeiculos(): Observable<VeiculosModel[]> {
-        return this.http.get<VeiculosModel[]>(this.path)
+    public getVeiculos(): Observable<VeiculoModel[]> {
+        return this.http.get<VeiculoModel[]>(this.url)
             .pipe(
-                tap(heroes => this.log(`fetched veiculos`)),
-                catchError(this.handleError('getHeroes', []))
+                tap(veiculos => this.log(`fetched veiculos`)),
+                catchError(this.handleError('get', []))
             );
     }
 
+    public getVeiculoId(id: number): Observable<VeiculoModel> {
+        const url = `${this.url}/${id}`;
+        return this.http.get<VeiculoModel>(url).pipe(
+            tap(_ => this.log(`fetched veículo id=${id}`)),
+            catchError(this.handleError<VeiculoModel>(`getVeiculo id=${id}`))
+        );
+    }
+
+    searchVeiculos(term: string = ''): Observable<VeiculoModel[]> {
+        //if (!term.trim()) {
+            // if not search term, return empty hero array.
+            //return of([]);
+        //}
+        return this.http.get<VeiculoModel[]>(`${this.url}/find?q=${term}`).pipe(
+            tap(_ => this.log(`found veiculos matching "${term}"`)),
+            catchError(this.handleError<VeiculoModel[]>('searchVeiculos', []))
+        );
+    }
+
+    public postVeiculo(veic: VeiculoModel): Observable<VeiculoModel> {
+        return this.http.post<VeiculoModel>(this.url, veic, httpOptions).pipe(
+            tap((result: VeiculoModel) => this.log(`veiculo adicionado w/ id=${result.id}`)),
+            catchError(this.handleError<VeiculoModel>('add veiculo'))
+        );
+    }
+
+    public putVeiculo(model: VeiculoModel): Observable<any> {
+        const url = `${this.url}/${model.id}`;
+        return this.http.put(url + '', model, httpOptions).pipe(
+            tap(_ => this.log(`updated veiculo id=${model.id}`)),
+            catchError(this.handleError<any>('updateVeiculo'))
+        );
+    }
+
+    public deleteVeiculo(model: VeiculoModel | number): Observable<VeiculoModel> {
+        const id = typeof model === 'number' ? model : model.id;
+        const url = `${this.url}/${id}`;
+
+        return this.http.delete<VeiculoModel>(url, httpOptions).pipe(
+            tap(_ => this.log(`deleted veículo id=${id}`)),
+            catchError(this.handleError<VeiculoModel>('deleteVeículo'))
+        );
+    }
+
+
     private log(message: string) {
-        this.messageService.add('HeroService: ' + message);
+        this.messageService.add('Service: ' + message);
     }
 
     private handleError<T>(operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
-            //console.error(error);
             this.log(`${operation} failed: ${error.message}`);
             return of(result as T);
         };
